861856c95ab1842c8fe523c4b20e7bff2c15728d
Saksham Tiwari <sakshamtiwari157@gmail.com>
2025-01-21 15:31:10 +0530

    Initial commit

---
+++ src/pages/Home/Section1.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Burger from "../../assets/hero/aa.png";
+import { Link } from "react-router-dom";
+
+const Section1 = () => {
+  return (
+    <section className="hero_section">
+      <Container>
+        <Row>
+          <Col lg={7} className="mb-5 mb-lg-0">
+            <div className="position-relative">
+              <img src={Burger} className="img-fluid" alt="Hero" />
+              <div className="price_badge">
+                <div className="badge_text">
+                  <h4 className="h4_xs">Only</h4>
+                  <h4 className="h3_lg">Rs.450</h4>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col lg={5}>
+            <div className="hero_text text-center">
+              <h1 className="text-white">PIZZA</h1>
+              <h2 className="text-white">CON CIPOLLA</h2>
+              <p className="text-white pt-2 pb-4"> 
+              A classic Italian pizza topped with caramelized onions, rich tomato sauce, and melted mozzarella cheese for a savory delight.
+              </p>
+              <Link to="/" className="btn order_now">
+                Order Now
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Section1;
---
+++ src/pages/Home/Section3.js
@@ -0,0 +1,153 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Image1 from "../../assets/menu/sa.png";
+import Image2 from "../../assets/menu/sa.png";
+import Image3 from "../../assets/menu/sa.png";
+import Image4 from "../../assets/menu/pzzzz.png";
+import Image5 from "../../assets/menu/burger-13.jpg";
+import Image6 from "../../assets/menu/burger-14.jpg";
+import Image7 from "../../assets/menu/burger-15.jpg";
+import Image8 from "../../assets/menu/burger-16.jpg";
+import Cards from "../../components/Layouts/Cards";
+import { Link } from "react-router-dom";
+
+// Mock Data Cards
+const mockData = [
+  {
+    id: "0001",
+    image: Image1,
+    title: "Margherita Pizza",
+    paragraph: "Chicken breast, chilli sauce, tomatoes, pickles, coleslaw",
+    rating: 5,
+    price: 99,
+  },
+  {
+    id: "0002",
+    image: Image2,
+    title: "Four Cheese Pizza ",
+    paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 4.5,
+    price: 99.32,
+  },
+  {
+    id: "0003",
+    image: Image3,
+    title: "Meat Lover’s Pizza",
+    paragraph: "American cheese, tomato relish, avocado, lettuce, red onion",
+    rating: 4,
+    price: 69.15,
+  },
+  {
+    id: "0004",
+    image: Image4,
+    title: "Chicken Tikka Pizza",
+    paragraph: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 3.5,
+    price: 99.25,
+  },
+  {
+    id: "0005",
+    image: Image5,
+    title: "Chicken BBQ Pizza",
+    paragraph: "2 patties, cheddar cheese, mustard, pickles, tomatoes",
+    rating: 3.0,
+    price: 59.25,
+  },
+  {
+    id: "0006",
+    image: Image6,
+    title: "Pepperoni Pizza",
+    paragraph: "Turkey, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 3,
+    price: 79.18,
+  },
+  {
+    id: "0007",
+    image: Image7,
+    title: "Hawaiian Pizza",
+    paragraph: "patty, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 2.5,
+    price: 99.19,
+  },
+  {
+    id: "0008",
+    image: Image8,
+    title: "Bacon and Mushroom Pizza",
+    paragraph: "cheddar cheese, ketchup, mustard, pickles, onion",
+    rating: 2.0,
+    price: 89.12,
+  },
+  // Add more mock data objects as needed
+];
+
+// Rating Logical Data
+const renderRatingIcons = (rating) => {
+  const stars = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (rating > 0.5) {
+      stars.push(<i key={i} className="bi bi-star-fill"></i>);
+      rating--;
+    } else if (rating > 0 && rating < 1) {
+      stars.push(<i key={"half"} className="bi bi-star-half"></i>);
+      rating--;
+    } else {
+      stars.push(<i key={`empty${i}`} className="bi bi-star"></i>);
+    }
+  }
+  return stars;
+};
+
+function Section3() {
+  return (
+    <section className="menu_section">
+      <Container>
+        <Row>
+          <Col lg={{ span: 8, offset: 2 }} className="text-center mb-5">
+            <h2>OUR CRAZY PIZZA'S & BURGER'S</h2>
+            <p className="para">
+              Aliquam a augue suscipit, luctus neque purus ipsum neque undo
+              dolor primis libero tempus, blandit a cursus varius magna
+            </p>
+          </Col>
+        </Row>
+        <Row className="card_row">
+          {mockData.map((cardData, index) => (
+            <Cards
+              key={index}
+              image={cardData.image}
+              rating={cardData.rating}
+              title={cardData.title}
+              paragraph={cardData.paragraph}
+              price={cardData.price}
+              renderRatingIcons={renderRatingIcons}
+            />
+          ))}
+        </Row>
+
+        <Row className="pt-5">
+          <Col sm={6} lg={5}>
+            <div className="ads_box ads_img1 mb-5 mb-md-0">
+              <h4 className="mb-0">GET YOUR FREE</h4>
+              <h5>CHEESE FRIES</h5>
+              <Link to="/" className="btn btn_red px-4 rounded-0">
+                Learn More
+              </Link>
+            </div>
+          </Col>
+          <Col sm={6} lg={7}>
+            <div className="ads_box ads_img2">
+              <h4 className="mb-0">GET YOUR FREE</h4>
+              <h5>CHEESE FRIES</h5>
+              <Link to="/" className="btn btn_red px-4 rounded-0">
+                Learn More
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Section3;
